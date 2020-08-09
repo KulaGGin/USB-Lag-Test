82a72962562fb25ac6117e6d73f66b88060fb637
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-09 18:51:01 +0300

    Fill LagTest.Arduino.ino sketch file.

--- LagTest.Arduino/LagTest.Arduino.ino
+++ LagTest.Arduino/LagTest.Arduino.ino
@@ -4,12 +4,19 @@
  Author:	Kulagin
 */
 
+#include <Keyboard.h>
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	Serial.begin(115200);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	while(true) {
+		if(Serial.available() > 0) {
+			Keyboard.write('a');
+			while(Serial.read() >= 0); //flush buffer
+		}
+	}
 }
