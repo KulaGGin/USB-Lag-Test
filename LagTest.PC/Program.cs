ff7cedb0221234c422de2df2b5399f9f1609745d
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-09 19:25:14 +0300

    Rename project LatTest to LagTest.

--- LagTest.PC/Program.cs
+++ LagTest.PC/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LatTest.PC {
+namespace LagTest.PC {
     class Program {
         static void Main(string[] args) {
             string testText = "Test Lag.";
