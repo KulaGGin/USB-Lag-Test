baf89d38b0b2ca4ad10487fd5eec5c6496ebc822
KulaGGin <sergeant.coolagin@gmail.com>
2020-08-09 18:25:18 +0300

    Add LagTest.Arduino project to the solution.

---
+++ LagTest.Arduino/LagTest.Arduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		LagTest.ino
+ Created:	09.08.2020 18:24:55
+ Author:	Kulagin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
